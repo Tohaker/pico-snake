236c629fdee4ffcb1026768c7b798ce4b04b7c24
Miles Bardon <milesbardon@gmail.com>
2021-01-24 14:48:52 +0000

    Adding function to create apple

--- main.cpp
+++ main.cpp
@@ -8,6 +8,35 @@ using namespace pimoroni;
 uint16_t buffer[PicoExplorer::WIDTH * PicoExplorer::HEIGHT];
 PicoExplorer pico_explorer(buffer);
 
+/**
+ * Draw an apple at the given coordinates.
+ *     GGG
+ *     G
+ *     G 
+ * RRRRRRRRRR
+ * RRRRRRRRRR
+ * RRRRRRRRRR
+ * RRRRRRRRRR
+ * RRRRRRRRRR
+ */
+void createApple(int x, int y)
+{
+    // Apple core
+    Rect apple(x, y, 10, 10);
+    pico_explorer.set_pen(214, 58, 47);
+    pico_explorer.circle(Point(x, y), 5);
+
+    // Stem
+    Rect stem(x, y - 8, 1, 3);
+    pico_explorer.set_pen(11, 110, 26);
+    pico_explorer.rectangle(stem);
+
+    // Leaf
+    Rect leaf(x, y - 8, 3, 1);
+    pico_explorer.set_pen(11, 110, 26);
+    pico_explorer.rectangle(leaf);
+}
+
 int main()
 {
     pico_explorer.init();
@@ -29,6 +58,9 @@ int main()
         Rect segment(x, y, 10, 10);
         pico_explorer.set_pen(255, 255, 255);
         pico_explorer.rectangle(segment);
+
+        // Decide apple location
+        createApple(PicoExplorer::WIDTH / 2, PicoExplorer::HEIGHT / 2);
 
         if (pico_explorer.is_pressed(pico_explorer.A))
         {
